fda20e56ee140f3a1b8d45927b9b47a169e5c8bf
NachooGG <igomezgalissier@mail.austral.edu.ar>
2025-04-14 12:43:09 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,6 @@
 def leap_year():
-    print("TO DO")
+    anio = int(input())
+    if (anio // 400) or (anio % 4 == 0) :
+        print(f"El año {anio} es bisiesto")
+    else : 
+        print(f"El año {anio} no es bisiesto"))
