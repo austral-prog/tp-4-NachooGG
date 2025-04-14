d16a3a5022461b81a78f0d499248df27b06871c7
NachooGG <igomezgalissier@mail.austral.edu.ar>
2025-04-14 14:49:22 -0300

    Update line.py

--- line.py
+++ line.py
@@ -1,23 +1,18 @@
-def line():
-    A = float(input())
-    B = float(input())
-    X1 = float(input())
-    X2 = float(input())
-    print(f"El coeficiente A de su ecuacion de la recta es: {A}")
-    print(f"El coeficiente B de su ecuacion de la recta es: {B}")
-    print(f"El coeficiente X1 de su ecuacion de la recta es: {X1}")
-    print(f"El coeficiente X2 de su ecuacion de la recta es: {X2}")
+def line(A, B, X1, X2):
+    print(f"El coeficiente A de su ecuación de la recta es: {A}")
+    print(f"El coeficiente B de su ecuación de la recta es: {B}")
+    print(f"El coeficiente X1 de su ecuación de la recta es: {X1}")
+    print(f"El coeficiente X2 de su ecuación de la recta es: {X2}")
 
-    print("\nPara la siguiente ecuacion:")
-    Y_1 = f"Y = {A}X + {B}"
-    
-    Y_3 = A * X1 + B
-    Y_4 = A * X2 + B
-    print(f"\t{Y_1}")
-  
-    
+    print("\nPara la siguiente ecuación:")
+    print(f"\tY = {A}X + {B}")
+
+    Y1 = A * X1 + B
+    Y2 = A * X2 + B
+
     print(f"\nDados los siguientes puntos:")
-    print(f"\tP1 ({X1}, {Y_3})")
-    print(f"\tP2 ({X2}, {Y_4})")
-    distancia = ((X2 - X1)**2 + (Y_4 - Y_3)**2)**(1/2)
-    print(f"\nLa distancia entre los dos puntos sobre la recta es: {distancia}")
+    print(f"\tP1 ({X1}, {Y1})")
+    print(f"\tP2 ({X2}, {Y2})")
+
+    distancia = ((X2 - X1)**2 + (Y2 - Y1)**2)**0.5
+    print(f"\nLa distancia entre ellos es: {distancia}")
